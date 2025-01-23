f3c53d47c611dcab0954ca0d6eecb916545ad39a
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-01-23 11:10:35 +0530

    layout update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div>
+      {/* https://www.youtube.com/watch?v=egS6fnZAdzk&ab_channel=RoadsideCoder @ 19:23 */}
       Hello World
     </div>
   );
