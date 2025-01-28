e1ae49d99a1ea5f8a11485ec8c74f9b7556a6cce
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-01-28 20:39:24 +0530

    updated with authentication, db and messageQ

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      {/* https://www.youtube.com/watch?v=egS6fnZAdzk&ab_channel=RoadsideCoder @ 19:23 */}
-      Hello World
-    </div>
-  );
+  return <div className="mt-40">{/* https://www.youtube.com/watch?v=egS6fnZAdzk&ab_channel=RoadsideCoder @ 47:57 */}</div>;
 }
